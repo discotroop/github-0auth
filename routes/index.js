00f8d61141c54d6de6073bececf2ff2c15e8fb70
John Mark Goodson <john.mark.hat@gmail.com>
2020-09-02 12:51:44 -0400

    added nodemon, plus dev scripts, acquired auth id and secret from gh, edited view templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
+
+// Client ID
+//     163f60273a1723c1dc1b
+// Client Secret
+//     1155d3dba48d06dd847b9768a3df09fd2598e1bf
